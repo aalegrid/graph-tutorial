ffecb2e27f12c3f5b5b03bd7cddf85961c264e3d
Alexander Alegrid <alexander.alegrid@health.telstra.com>
2023-08-13 17:26:10 +1000

    first commit

---
+++ graph-tutorial/Models/CachedUser.cs
@@ -0,0 +1,11 @@
+﻿
+namespace graph_tutorial.Models
+{
+    // Simple class to serialize user details
+    public class CachedUser
+    {
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+        public string Avatar { get; set; }
+    }
+}
